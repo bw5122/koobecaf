5e879b83bdd18f50367eb2e78e95a350efa4b791
阎涵 <yanhan166@gmail.com>
2018-12-08 22:36:39 -0500

    posts and comment testing

--- client/src/Components/Post.js
+++ client/src/Components/Post.js
@@ -8,12 +8,20 @@ class Post extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: [{},{}],
+      comments: [],
       newcomment: '',
-      likes: {} // list of user names
+      likes: {}, // list of user names
     };
+    this.handleChange = this.handleChange.bind(this);
     this.handleNewComment = this.handleNewComment.bind(this);
     this.handleNewLike = this.handleNewLike.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      comments: this.props.info.comments,
+      userID: this.props.userID
+    });
   }
 
   handleChange(event) {
@@ -24,25 +32,33 @@ class Post extends Component {
 
   handleNewComment = e =>{
     e.preventDefault();
-    fetch("/post/addcomment" + this.state.userID, {
+    fetch("/post/addcomment", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
+        type: "comment",
         postID: this.props.info.postID,
         content: this.state.newcomment,
-        creator: this.props.postBy
+        creator: this.state.userID
       })
     })
       .then(res => res.json())
       .then(
-        result => {
-          this.setState(prevState => ({
-            comments: [...prevState.comments, result]
-          }))
+        (result) => {
+          if(this.state.comments) {
+            this.setState({
+              comments: [...this.state.comments, result.data]
+            });
+          } else {
+            this.setState({
+              comments: [result.data]
+            });
+          }
+
         },
-        error => {
+        (error) => {
           alert("Error (add comment)! Please try again.");
         }
       );
@@ -58,18 +74,18 @@ class Post extends Component {
 
   render() {
     const time = (this.props.info) ? (this.props.info).createdAt : '';
-    const comments = this.state.comments.map((ele) =>
-      <Comment info={ele} />
-    );
+    const all_comments = (this.state.comments) ? this.state.comments.map((ele) =>
+      <Comment info={ele} userID={this.state.userID}/>
+    ) : [];
     return (
       <div className="box">
         <h3>{this.props.info.creator}</h3>
         <Moment date={time} />
         <p>{this.props.info.content}</p>
         <button id="like_button" onClick={this.handleNewLike}>like</button>
-        <ul id="comment_list">{comments}</ul>
+        <ul id="comment_list">{all_comments}</ul>
         <form className="createcomment" onSubmit={this.handleNewComment}>
-          <input type="text" placeholder="Write a comment..." id="newcomment" value={this.state.newcomment.value}  onChange={this.handleChange} maxLength="100" />
+          <input type="text" name="newcomment" placeholder="Write a comment..." id="newcomment" value={this.state.newcomment.value}  onChange={this.handleChange} maxLength="100" />
           <input type="submit" id="comment_button" value="Comment" />
         </form>
       </div>
