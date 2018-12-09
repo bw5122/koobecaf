6e92165d5c270ced0306f654491ad91014625dc4
阎涵 <yanhan166@gmail.com>
2018-12-09 16:18:25 -0500

    unlike

--- client/src/Components/Post.js
+++ client/src/Components/Post.js
@@ -7,21 +7,27 @@ import '../Styles/Post.css';
 class Post extends Component {
   constructor(props) {
     super(props);
+    console.log(props.info.comments);
     this.state = {
-      comments: [],
+      comments: props.info.comments,
       newcomment: '',
-      likes: {}, // list of user names
+      likes: props.info.likes, // list of user names
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleNewComment = this.handleNewComment.bind(this);
     this.handleNewLike = this.handleNewLike.bind(this);
+    this.handleShare = this.handleShare.bind(this);
   }
 
   componentDidMount() {
-    this.setState({
-      comments: this.props.info.comments,
-      userID: this.props.userID
+    const id = this.props.userInfo.userID;
+    var b = false;
+    this.state.likes.map(function(like_obj){
+      if(like_obj.creator.userID == id) {
+        b = true;
+      }
     });
+    this.setState({liked: b});
   }
 
   handleChange(event) {
@@ -41,7 +47,7 @@ class Post extends Component {
         type: "comment",
         postID: this.props.info.postID,
         content: this.state.newcomment,
-        creator: this.state.userID
+        creator: this.props.userInfo.userID
       })
     })
       .then(res => res.json())
@@ -64,25 +70,100 @@ class Post extends Component {
       );
   }
 
-  handleDeleteComment = e => {
+  handleDeleteComment(e) {
     //no data returned.
   }
 
-  handleNewLike = e => {
+  handleShare(e) {
+    e.preventDefault();
+    
+  }
+
+  handleNewLike(e) {
+    e.preventDefault();
+    if(this.state.liked) {
+      fetch("/post/unlikepost", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          type: "like",
+          postID: this.props.info.postID,
+          creator: this.props.userInfo.userID
+        })
+      })
+      .then(res => res.json())
+      .then(
+        (res) => {
+          //remove user's like
+          var new_arr = this.state.likes.filter((like_obj) =>
+            like_obj.creator.userID !== this.props.userInfo.userID
+          )
+          this.setState({
+            likes: new_arr,
+            liked: !this.state.liked
+          });
+        },
+        (error) => {
+          alert("error (unlike)");
+        }
+      )
+    } else {
+      fetch("/post/likepost", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          type: "like",
+          postID: this.props.info.postID,
+          creator: this.props.userInfo.userID
+        })
+      })
+      .then(res => res.json())
+      .then(
+        (res) => {
+          const id = res.data.creator;
+          res.data.creator = {
+            userID: id,
+            firstname: this.props.userInfo.firstname,
+            lastname: this.props.userInfo.lastname
+          }
+          this.setState({
+            likes: [res.data, ...this.state.likes],
+            liked: !this.state.liked
+          });
+          //console.log(this.state.post.size);
+        },
+        (error) => {
+          alert("error (new like)");
+        }
+      )
+    }
 
   }
+
 
   render() {
     const time = (this.props.info) ? (this.props.info).createdAt : '';
     const all_comments = (this.state.comments) ? this.state.comments.map((ele) =>
-      <Comment info={ele} userID={this.state.userID}/>
+      <Comment info={ele} userInfo={this.props.userInfo}/>
     ) : [];
+    console.log(this.state.likes);
+
     return (
       <div className="box">
-        <h3>{this.props.info.creator}</h3>
+        <h3>{this.props.info.postBy.firstname} {this.props.info.postBy.lastname}</h3>
         <Moment date={time} />
         <p>{this.props.info.content}</p>
-        <button id="like_button" onClick={this.handleNewLike}>like</button>
+        {this.state.likes.length > 0 &&
+          <h4>{this.state.likes[0].creator.firstname} {this.state.likes[0].creator.lastname} and {this.state.likes.length - 1} friends like it</h4>
+        }
+        <button id="like_button" onClick={this.handleNewLike}>
+          {this.state.liked ? 'unlike' : 'like'}
+        </button>
+        <button id="share_button" onClick={this.handleShare}>share</button>
         <ul id="comment_list">{all_comments}</ul>
         <form className="createcomment" onSubmit={this.handleNewComment}>
           <input type="text" name="newcomment" placeholder="Write a comment..." id="newcomment" value={this.state.newcomment.value}  onChange={this.handleChange} maxLength="100" />
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -10,10 +10,7 @@ class Home extends Component {
     this.state = {
       // all fields limit length to 20 characters
       userInfo: this.props.location.state.userInfo,
-      posts: [{postBy: 'jack',
-              creator: 'jack',
-              content: 'test post',
-              friendtags: ['berry']}],
+      posts: [],
       newpost: '',
       friendtags: []
 
@@ -57,8 +54,6 @@ class Home extends Component {
     .then(res => res.json())
     .then(
       (res) => {
-        res.data.comments = [];
-        console.log(res.data);
         this.setState({
           posts: [res.data, ...this.state.posts]
         });
@@ -78,6 +73,7 @@ class Home extends Component {
     .then(
       (res) => {
         this.setState({posts: res.data});
+        console.log(this.state.posts);
       },
       (error) => {
         console.log(error);
@@ -88,10 +84,11 @@ class Home extends Component {
 //TODO: share button
   render() {
     const username = this.props.location.state.username;
-
-    const all_posts = this.state.posts.map((post) =>
-      <Post info={post} userID={this.state.userID}/>
+    const posts = this.state.posts;
+    const all_posts = posts.map((post) =>
+      <Post info={post} userInfo={this.state.userInfo} />
     );
+    
     return(
       <div className="homepage">
         <div className="nav">
@@ -109,7 +106,7 @@ class Home extends Component {
               <input type="submit" id="create_button" value="Share" />
             </form>
             <div className="oldposts">
-            <ul>{all_posts}</ul>
+            <div>{all_posts}</div>
             </div>
           </div>
         </div>
