16411072739f95c6ec04fca6761cfc910786b552
Chiaki <Chiaki530@outlook.com>
2018-12-14 15:25:09 -0500

    add denyfriend

--- server/routers/friend.js
+++ server/routers/friend.js
@@ -8,6 +8,7 @@ router.get('/getfriend/:userID', friend_ctrl.get_friend);
 router.post('/sendfriendrequest', friend_ctrl.send_friend_request);
 router.get('/getfriendrequest/:userID', friend_ctrl.get_friend_request);
 router.post('/acceptfriend', friend_ctrl.add_friend);
+router.delete('/denyfriend', friend_ctrl.deny_friend);
 router.get('/generaterelationgraph', graph_ctrl.generate_graph);
 router.get('/visualizefriend/:userID', graph_ctrl.visualize_friend);
 router.get('/visualizemorefriend/:userID/:friendID', graph_ctrl.visualize_morefriend);
